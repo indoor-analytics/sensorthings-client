b4e743086df12a895708ba5689a89faf7a4422e6
Remy Raes <raes.remy@gmail.com>
2021-09-27 18:22:19 +0200

    [feat] SensorThingsService has a HTTP client to do requests

--- src/service/SensorThingsService.ts
+++ src/service/SensorThingsService.ts
@@ -1,15 +1,24 @@
 import {Entity} from "../model/Entity";
+import axios, {AxiosInstance} from "axios";
 
 export class SensorThingsService {
     private readonly _endpoint: URL;
+    private _httpClient: AxiosInstance;
     constructor (endpoint: URL) {
         this._endpoint = endpoint;
+        this._httpClient = axios.create({
+            baseURL: this._endpoint.toString()
+        });
     }
     get endpoint (): URL {
         return this._endpoint;
     }
+    set httpClient (client: AxiosInstance) {
+        this._httpClient = client;
+    }
 
     public create (entity: Entity): void {
+        this._httpClient.post("");
         throw Error(`Not implemented: ${entity}`);
     }
 
