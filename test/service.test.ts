ac0f22ee78857655eacaa07addc00a84a1046561
Remy Raes <raes.remy@gmail.com>
2021-09-27 17:56:42 +0200

    [test] SensorThingsService should not accept random string as endpoint

--- test/service.test.ts
+++ test/service.test.ts
@@ -5,4 +5,9 @@ describe('SensorThingsService', () => {
         const build = () => new SensorThingsService(new URL(''));
         expect(build).toThrowError(new TypeError('Invalid URL: '));
     });
+
+    it ('should not accept random string as endpoint', () => {
+        const build = () => new SensorThingsService(new URL('this_is_my_endpoint'));
+        expect(build).toThrowError(/Invalid URL:/);
+    });
 });
