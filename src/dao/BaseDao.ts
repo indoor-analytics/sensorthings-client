6eed33bf8ebb94adcb3def8d24b8344657c5f72a
Rémy Raes <contact@remyraes.com>
2021-09-28 15:21:33 +0200

    [feat] adding basic DAO

---
+++ src/dao/BaseDao.ts
@@ -0,0 +1,23 @@
+import {Entity} from "../model/Entity";
+import {SensorThingsService} from "../service/SensorThingsService";
+
+export abstract class BaseDao<T extends Entity> {
+    private _service: SensorThingsService;
+
+    protected constructor(service: SensorThingsService) {
+        this._service = service;
+    }
+
+
+    public async create (entity: T): Promise<void> {
+        return this._service.create(entity);
+    }
+
+    public async update (entity: T): Promise<void> {
+        return this._service.update(entity);
+    }
+
+    public async delete (entity: T): Promise<void> {
+        return this._service.delete(entity);
+    }
+}
