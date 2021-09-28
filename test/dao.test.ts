686dcf66928263d9257fb9503b4c1ee33a5c4ccb
Rémy Raes <contact@remyraes.com>
2021-09-28 16:12:33 +0200

    [test] ThingDao should return correct path name

---
+++ test/dao.test.ts
@@ -0,0 +1,13 @@
+import {ThingDao} from "../src/dao/ThingDao";
+import {SensorThingsService} from "../src";
+
+describe('DAO', () => {
+    describe('Entity path names', () => {
+        const service = new SensorThingsService('https://example.org');
+
+        it('ThingDao should return correct path name', () => {
+            const urlPrefix = new ThingDao(service).getEntityPathname();
+            expect(urlPrefix).toEqual('Things');
+        });
+    })
+});
