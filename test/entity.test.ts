b3c9771c43224e3b05e33e2cc38db5ff629f4ed9
Rémy Raes <contact@remyraes.com>
2021-09-28 11:20:22 +0200

    [test] Entity shouldn't return id when not created

---
+++ test/entity.test.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import {MockEntity} from "./utils/MockEntity";
+
+describe ('Entity', () => {
+    it ('shouldn\'t return id when not created', () => {
+        const payload = new MockEntity('name', 'description');
+        const getId = () => payload.id;
+        expect(getId).toThrowError(new RangeError('Entity hasn\'t been created on a service yet.'));
+    })
+});
