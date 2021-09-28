7ab26e697b967138681587a2bf220e3a0d6a43cd
Rémy Raes <contact@remyraes.com>
2021-09-28 13:51:27 +0200

    [test] Entity should not return pathname when not created

--- test/entity.test.ts
+++ test/entity.test.ts
@@ -9,4 +9,12 @@ describe('Entity', () => {
             new RangeError("Entity hasn't been created on a service yet.")
         );
     });
+
+    it ('should not return pathname when not created', () => {
+        const payload = new MockEntity('name', 'description');
+        const getLink = () => payload.entityResourcePathname;
+        expect(getLink).toThrowError(
+            new RangeError("Entity hasn't been created on a service yet.")
+        );
+    });
 });
