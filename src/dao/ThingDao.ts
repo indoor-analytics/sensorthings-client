ce722a7243c3ef8512b7dd515464c1687bccb05f
Rémy Raes <contact@remyraes.com>
2021-09-29 13:27:54 +0200

    [feat] DAO throws when getting non-existent entity

--- src/dao/ThingDao.ts
+++ src/dao/ThingDao.ts
@@ -1,5 +1,7 @@
 import { BaseDao } from './BaseDao';
 import { Thing } from '../model/Thing';
+import {NotFoundError} from "../error/NotFoundError";
+import {AxiosError, AxiosResponse} from "axios";
 
 export class ThingDao extends BaseDao<Thing> {
     getEntityPathname(): string {
@@ -7,12 +9,20 @@ export class ThingDao extends BaseDao<Thing> {
     }
 
     async get(id: number): Promise<Thing> {
-        const response = await this._service.httpClient.get([
+        return await this._service.httpClient.get([
             this._service.endpoint.origin,
             this.getEntityPathname() + `(${id})`
-        ].join('/'));
-        const thing = new Thing(response.data.name, response.data.description);
-        thing.id = response.data['@iot.id']
-        return thing;
+        ].join('/'))
+            .catch((error: AxiosError) => {
+                if (error.response?.status === 404) {
+                    throw new NotFoundError('Entity does not exist.');
+                }
+
+                // @ts-ignore
+        }).then((response: AxiosResponse) => {
+            const thing = new Thing(response.data.name, response.data.description);
+            thing.id = response.data['@iot.id']
+            return thing;
+        });
     }
 }
