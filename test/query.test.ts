e9f3f387df6d9c611f35c28bbee045e6f441a73d
Remy Raes <contact@remyraes.com>
2021-10-04 18:05:12 +0200

    [test] Query<DumbEntity> should return DumbEntity endpoint

--- test/query.test.ts
+++ test/query.test.ts
@@ -2,8 +2,16 @@ import {SensorThingsService} from "../src";
 import {Query} from "../src/query/Query";
 import {Thing} from "../src/model/Thing";
 import {ThingDao} from "../src/dao/ThingDao";
+import { DumbEntity } from "./utils/DumbEntity";
+import { DumbEntityDao } from "./utils/DumbEntityDao";
 
 describe('Query', () => {
+    it('Query<DumbEntity> should return DumbEntity endpoint', () => {
+        const service = new SensorThingsService('https://example.org/v1.0');
+        const query = new Query<DumbEntity>(service, new DumbEntityDao(service));
+        expect(query.endpoint).toEqual('https://example.org/v1.0/DumbEntities');
+    });
+    
     it('Query<Thing> should return Things endpoint', () => {
         const service = new SensorThingsService('https://example.org/v1.0');
         const query = new Query<Thing>(service, new ThingDao(service));
