c3cd115dda4454534d295df6e28c1eea965ed0a7
Rémy Raes <contact@remyraes.com>
2021-10-04 15:21:20 +0200

    [test] adding Thing operations in integration test

--- test/integration.ts
+++ test/integration.ts
@@ -1,11 +1,25 @@
 import { SensorThingsService } from '../src';
+import {Thing} from "../src/model/Thing";
 
 async function main() {
     const service = new SensorThingsService(
-        'http://scratchpad.sensorup.com/OGCSensorThings/v1.0'
+        'http://localhost:8080/v1.0'
     );
-    const thing = await service.things.get(2708208);
-    console.log(thing);
+
+    // Getting non-existent Thing
+    try {
+        const thing = await service.things.get(8708208);
+        console.log(thing.name);
+    } catch (err) {
+        console.log(err);
+    }
+
+    // Creating a new Thing
+    const newThing = new Thing('Test thing', 'Test description');
+    await service.things.create(newThing);
+
+    // Retrieving Thing from API
+    console.log(await service.things.get(newThing.id));
 }
 
 main();
