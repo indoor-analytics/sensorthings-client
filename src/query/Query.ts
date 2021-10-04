926f0e05dce4846818c7b06b23efa398fb28584f
Rémy Raes <contact@remyraes.com>
2021-10-04 16:39:42 +0200

    [feat] adding basic Query class skeleton

--- src/query/Query.ts
+++ src/query/Query.ts
@@ -1,5 +1,15 @@
 import {Entity} from "../model/Entity";
+import {SensorThingsService} from "../service/SensorThingsService";
 
 export class Query<T extends Entity<T>> {
+    private _service: SensorThingsService;
 
+    constructor (service: SensorThingsService) {
+        this._service = service;
+        console.log(this._endpoint);
+    }
+
+    private get _endpoint (): string {
+        return [this._service.endpoint, '// TODO'].join('/');
+    }
 }
