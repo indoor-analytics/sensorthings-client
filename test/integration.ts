2b1805752f634c907cc746009f8a5c6588b4e9f7
Remy Raes <contact@remyraes.com>
2021-10-04 08:30:53 +0200

    [refactor] lint

--- test/integration.ts
+++ test/integration.ts
@@ -1,9 +1,11 @@
-import { SensorThingsService } from "../src";
+import { SensorThingsService } from '../src';
 
-async function main () {
-    const service = new SensorThingsService("http://scratchpad.sensorup.com/OGCSensorThings/v1.0");
+async function main() {
+    const service = new SensorThingsService(
+        'http://scratchpad.sensorup.com/OGCSensorThings/v1.0'
+    );
     const thing = await service.things.get(2708208);
     console.log(thing);
 }
 
-main ();
+main();
