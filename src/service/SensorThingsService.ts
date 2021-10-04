0418ddce30cf792e7b8bf818566fd6b8cb809909
Rémy Raes <contact@remyraes.com>
2021-10-04 16:13:33 +0200

    [docs] adding documentation to service.url/endpoint

--- src/service/SensorThingsService.ts
+++ src/service/SensorThingsService.ts
@@ -15,9 +15,17 @@ export class SensorThingsService {
             url: this._url.href
         });
     }
+
+    /**
+     * Returns the URL where this service is accessible.
+     */
     get url(): URL {
         return this._url;
     }
+    /**
+     * Returns this service's endpoint URL without final slash.
+     * This is used to compute URLs to associated entities.
+     */
     get endpoint(): string {
         const endpoint = this.url.toString();
         return endpoint.charAt(endpoint.length-1) === '/'
@@ -37,6 +45,7 @@ export class SensorThingsService {
     public async delete<T extends Entity<T>> (entity: T): Promise<void> {
         return entity.getDao(this).delete(entity);
     }
+
 
     public get things(): ThingDao {
         return new ThingDao(this);
