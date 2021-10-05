b9ee925b9475cf67492eb3f985a61f49d9360652
Rémy Raes <contact@remyraes.com>
2021-10-05 12:40:30 +0200

    [chore] linter warns on windows-style linebreaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,6 @@ module.exports = {
         "@typescript-eslint/ban-ts-comment": "off",
         "indent": ["error", 4],
         "@typescript-eslint/ban-types": "off",
-        "linebreak-style": ["error", "unix"]
+        "linebreak-style": ["warn", "unix"]
     }
 };
