52df9b1816e26d89140f369f06606a4449a88d3d
Remy Raes <contact@remyraes.com>
2021-10-08 16:34:19 +0200

    [test] Query.orderBy should order by name with suffix asc

--- test/query.test.ts
+++ test/query.test.ts
@@ -223,6 +223,29 @@ describe('Query', () => {
                 new IncorrectExpressionError('"azerty" is not a valid OrderBy expression.')
             );
         });
+
+        it ('should order by name with suffix "asc"', async () => {
+            const service = new SensorThingsService('https://example.org/v1.0');
+            const query = new DumbQuery<DumbEntity>(service, new DumbEntityDao(service));
+            mockInjector.injectMockCall(service, `https://example.org/v1.0/DumbEntities?$orderBy=name asc`, 'get', () => {
+                return {
+                    data: ThingAPIResponses.getThingsOrderedBy("name")
+                }
+            });
+
+            const entities = await query
+                .orderBy('name asc')
+                .list();
+
+            for (let i=0; i<entities.length-1; i++) {
+                const firstItem = entities[i];
+                const secondItem = entities[i+1];
+                if (firstItem.name.localeCompare(secondItem.name) > 0) {
+                    fail('Items are not sorted by name.');
+                    return;
+                }
+            }
+        });
     });
 
     describe('Combining query operations', () => {
