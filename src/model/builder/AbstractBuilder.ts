09aaa0cfc1d12a0d7e107a58c3714de3f6316576
Remy Raes <contact@remyraes.com>
2021-10-14 21:33:57 +0200

    [docs] adding documentation to AbstractBuilder

--- src/model/builder/AbstractBuilder.ts
+++ src/model/builder/AbstractBuilder.ts
@@ -1,6 +1,11 @@
 import {Entity} from "../Entity";
 import {SensorThingsService} from "../../service/SensorThingsService";
 
+/**
+ * Entity independant implementation of an entity builder object.
+ * It allows to create entity instances by injecting properties and associated
+ * service reference.
+ */
 export abstract class AbstractBuilder<T extends Entity<T>> {
     protected _attributes: Record<string, unknown> = {};
     protected _service: SensorThingsService;
@@ -8,5 +13,9 @@ export abstract class AbstractBuilder<T extends Entity<T>> {
         this._service = service;
         this._attributes = {};
     }
+    /**
+     * Returns an entity of type T with all specified properties.
+     * Throws if a needed property has not been set on this builder.
+     */
     abstract build(): T;
 }
