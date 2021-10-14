3af38a8dac95b27502a397dcd05e0a142723cfc6
Rémy Raes <contact@remyraes.com>
2021-10-14 11:34:20 +0200

    [test] builder should not build with missing name

--- test/builder.test.ts
+++ test/builder.test.ts
@@ -1,5 +1,6 @@
 import {SensorThingsService} from "../src";
 import {ThingBuilder} from "../src/model/builder/ThingBuilder";
+import {MissingArgumentError} from "../src/error/MissingArgumentError";
 
 describe('Model builders', () => {
     it('should build a Thing entity', () => {
@@ -13,5 +14,17 @@ describe('Model builders', () => {
             .build();
         expect(thing.name).toEqual(name);
         expect(thing.description).toEqual(description);
+    });
+
+    it('should not build with missing name', () => {
+        const builder = new ThingBuilder( new SensorThingsService('https://example.org') );
+        const description = 'This is a test description';
+
+        const build = () => builder
+            .setDescription(description)
+            .build();
+        expect(build).toThrowError(
+            new MissingArgumentError('"name" argument is missing.')
+        );
     });
 });
