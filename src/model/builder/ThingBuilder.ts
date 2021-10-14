13c5dc213ba0c8e88e5de7008e5e4b516d561fe0
Rémy Raes <contact@remyraes.com>
2021-10-14 11:37:56 +0200

    [feat] builder throws if an argument is missing

--- src/model/builder/ThingBuilder.ts
+++ src/model/builder/ThingBuilder.ts
@@ -1,6 +1,7 @@
 import {AbstractBuilder} from "./AbstractBuilder";
 import {Thing} from "../Thing";
 import {SensorThingsService} from "../../service/SensorThingsService";
+import {MissingArgumentError} from "../../error/MissingArgumentError";
 
 export class ThingBuilder extends AbstractBuilder<Thing> {
     constructor(service: SensorThingsService) {
@@ -17,6 +18,11 @@ export class ThingBuilder extends AbstractBuilder<Thing> {
     }
 
     build(): Thing {
+        if (!this._attributes.name)
+            throw new MissingArgumentError('"name" argument is missing.');
+        if (!this._attributes.description)
+            throw new MissingArgumentError('"description" argument is missing.');
+
         const t: Thing = new Thing('' + this._attributes.name, '' + this._attributes.description);
         t.setService(this._service);
         return t;
