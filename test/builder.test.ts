256956543f4eed38f15c3c80211d3321eadcf916
Remy Raes <contact@remyraes.com>
2021-10-14 21:48:21 +0200

    [test] builders should not retain attributes between consecutive entity builds

--- test/builder.test.ts
+++ test/builder.test.ts
@@ -39,4 +39,21 @@ describe('Model builders', () => {
             new MissingArgumentError('"description" argument is required to build a Thing.')
         );
     });
+
+    it('should not retain attributes between consecutive entity builds', () => {
+        const builder = new ThingBuilder( new SensorThingsService('https://example.org') );
+        const thing1 = builder
+            .setName('name1')
+            .setDescription('description1')
+            .build();
+        expect(thing1.name).toEqual('name1');    
+
+        const build = () => builder
+            .setName('name2')
+            .build();
+
+        expect(build).toThrowError(
+            new MissingArgumentError('"description" argument is required to build a Thing.')
+        );
+    });
 });
