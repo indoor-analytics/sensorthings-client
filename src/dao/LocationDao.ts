1ce4b1bca6966f7011fbec7a4a188a1f1f04b96b
Rémy Raes <contact@remyraes.com>
2021-10-18 11:39:20 +0200

    [feat] adding location to Location public attributes

--- src/dao/LocationDao.ts
+++ src/dao/LocationDao.ts
@@ -1,20 +1,20 @@
 import {BaseDao} from "./BaseDao";
 import {Location} from "../model/Location";
-import {Feature} from "@turf/helpers";
+import {Feature, Geometry} from "@turf/helpers";
 
 export class LocationDao extends BaseDao<Location> {
     buildEntityFromSensorThingsAPI(data: Record<string, unknown>): Location {
         const location = new Location(
             data.name as string,
             data.description as string,
-            data.location as Feature,
+            data.location as Feature<Geometry>,
             this._service
         );
         location.id = data['@iot.id'] as unknown as number;
         return location;
     }
     get entityPublicAttributes(): string[] {
-        return ['name', 'description', 'properties'];
+        return ['name', 'description', 'properties', 'location'];
     }
     get entityPathname(): string {
         return "Locations";
