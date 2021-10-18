7a65841136399498dd174b14fb8d86e4929cd80a
Rémy Raes <contact@remyraes.com>
2021-10-18 16:23:44 +0200

    [test] rewriting integration test suite with jest formatting

--- test/integration.ts
+++ test/integration.ts
@@ -1,54 +1,62 @@
 import { SensorThingsService } from '../src';
 import {Thing} from "../src/model/Thing";
+import {Location} from "../src/model/Location";
 import {LocationBuilder} from "../src/model/builder/LocationBuilder";
+import {NotFoundError} from "../src/error/NotFoundError";
 
-async function main() {
-    // FROST-Server SensorThings service
-    const service = new SensorThingsService(
-        'http://localhost:8080/FROST-Server/v1.0'
-    );
-    // Gost SensorThings service
-    /*
-    const service = new SensorThingsService(
-        'http://localhost:8080/v1.0',
-        {
-            locationEncodingType: 'application/vnd.geo+json'
-        }
-    );
-     */
-
-    // Getting non-existent Thing
-    console.log('\n## Getting non-existent Thing\n');
-    try {
-        const thing = await service.things.get(8708208);
-        console.log(thing.name);
-    } catch (err) {
-        console.log(err);
+// FROST-Server SensorThings service
+const service = new SensorThingsService(
+    'http://localhost:8080/FROST-Server/v1.0'
+);
+// Gost SensorThings service
+/*
+const service = new SensorThingsService(
+    'http://localhost:8080/v1.0',
+    {
+        locationEncodingType: 'application/vnd.geo+json'
     }
+);
+ */
 
-    // Creating a new Thing
-    console.log('\n## Creating a new Thing\n');
-    const newThing = new Thing('Test thing', 'Test description', service);
-    await service.things.create(newThing);
+describe('Integration tests', () => {
+    let thing = null as unknown as Thing;
+    let location = null as unknown as Location;
 
-    // Retrieving Thing from API
-    console.log('' + await service.things.get(newThing.id));
+    it('should throw on getting non-existent Thing', async () => {
+        const getThing = async () => thing = await service.things.get(8708208);
+        await expect(getThing()).rejects.toThrow(
+            new NotFoundError('Entity does not exist.')
+        );
+    });
 
-    // Pushing a new location
-    console.log('\n## Pushing a new location\n');
-    const location = new LocationBuilder(service)
-        .setName('Test thing location')
-        .setDescription('Is... Is it moving?!')
-        .setProperties({"hello": "there"})
-        .setLocation([50.6048862, 3.1498135])
-        .build();
-    await newThing.locations.add(location);
-    console.log('' + await newThing.locations.list());
+    it('should create a thing', async () => {
+        thing = new Thing('Test thing', 'Test description', service);
+        await service.things.create(thing);
+        expect(thing.id).not.toBeUndefined();
+    });
 
-    // Retrieving thing from posted location
-    console.log('' + await location.things.list());
+    it('should equal API-imported thing', async () => {
+        const importedThing = await service.things.get(thing.id);
+        expect(importedThing).toEqual(thing);
+    });
 
-    console.log('\n\nOK!')
-}
+    it('should push a new location', async () => {
+        const newLocation = new LocationBuilder(service)
+            .setName('Test thing location')
+            .setDescription('Is... Is it moving?!')
+            .setProperties({"hello": "there"})
+            .setLocation([50.6048862, 3.1498135])
+            .build();
+        await thing.locations.add(newLocation);
+        const list = await thing.locations.list();
+        expect(list.length).toEqual(1);
+        location = newLocation;
+    });
 
-main();
+    it('should list location\'s things', async() => {
+        const thingsList = await location.things.list();
+        expect(thingsList.length).toEqual(1);
+        expect(thingsList[0]).toEqual(thing);
+    });
+});
+
