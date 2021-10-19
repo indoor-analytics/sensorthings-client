1479d9c881a03b0ba9312fb203f3c3fb38339f25
Rémy Raes <contact@remyraes.com>
2021-10-19 16:04:25 +0200

    [feat] Thing has its HistoricalLocations list

--- src/model/Thing.ts
+++ src/model/Thing.ts
@@ -3,6 +3,7 @@ import { BaseDao } from '../dao/BaseDao';
 import { ThingDao } from '../dao/ThingDao';
 import { SensorThingsService } from '../service/SensorThingsService';
 import {ThingLocationsList} from "./list/ThingLocationsList";
+import {ThingHistoricalLocationsList} from "./list/ThingHistoricalLocationsList";
 
 /**
  * Representation of a Thing SensorThings entity.
@@ -12,12 +13,14 @@ export class Thing extends Entity<Thing> {
     public name: string;
     public description: string;
     public locations: ThingLocationsList;
+    public historicalLocations: ThingHistoricalLocationsList;
 
     constructor(name: string, description: string, service: SensorThingsService) {
         super(service);
         this.name = name;
         this.description = description;
         this.locations = new ThingLocationsList(this, this._service);
+        this.historicalLocations = new ThingHistoricalLocationsList(this, this._service);
     }
 
     get dao(): BaseDao<Thing> {
