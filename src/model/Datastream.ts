cc76320d91c6db182cf91653ae0f403dc9d1a623
Remy Raes <raes.remy@gmail.com>
2021-10-27 14:52:45 +0200

    [feat] adding Datastream constructor

--- src/model/Datastream.ts
+++ src/model/Datastream.ts
@@ -1,6 +1,7 @@
 import {Entity} from "./Entity";
 import {BaseDao} from "../dao/BaseDao";
 import {Polygon} from "@turf/helpers";
+import {SensorThingsService} from "../service/SensorThingsService";
 
 export class Datastream extends Entity<Datastream> {
     public name: string;
@@ -16,6 +17,18 @@ export class Datastream extends Entity<Datastream> {
     //public sensors;
     //public observedProperties;
     //public observations;
+
+    constructor(service: SensorThingsService, name: string, description: string, unitOfMeasurement: {name: string, symbol: string, definition: string}, observationType: 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_TruthObservation',
+                observedArea: Polygon, phenomenonTime: string, resultTime: string) {
+        super(service);
+        this.name = name;
+        this.description = description;
+        this.unitOfMeasurement = unitOfMeasurement;
+        this.observationType = observationType;
+        this.observedArea = observedArea;
+        this.phenomenonTime = phenomenonTime;
+        this.resultTime = resultTime;
+    }
 
     get dao(): BaseDao<Datastream> {
         return undefined;
