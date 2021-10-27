d4df618542f6e123ecfd43898561d2457ed299f7
Remy Raes <raes.remy@gmail.com>
2021-10-28 00:21:44 +0200

    [refactor] DAOs implement only buildEntity method instead of two raw build methods

--- src/dao/HistoricalLocationDao.ts
+++ src/dao/HistoricalLocationDao.ts
@@ -1,4 +1,3 @@
-import { AxiosResponse } from "axios";
 import { BaseDao } from "./BaseDao";
 import { HistoricalLocation } from "../model/HistoricalLocation";
 
@@ -9,22 +8,10 @@ export class HistoricalLocationDao extends BaseDao<HistoricalLocation> {
     get entityPublicAttributes(): string[] {
         return ['time'];
     }
-    buildEntityFromSensorThingsAPIRawData(data: Record<string, string>): HistoricalLocation {
-        const location = new HistoricalLocation(
+    buildEntity(data: Record<string, unknown>): HistoricalLocation {
+        return new HistoricalLocation(
             this._service,
-            data.time
+            data.time as string
         );
-        location.id = data['@iot.id'] as unknown as number;
-        return location;
     }
-    buildEntityFromSensorThingsAPIResponse(response: AxiosResponse<never>): HistoricalLocation {
-        const data = response.data as Record<string, string>;
-        const location = new HistoricalLocation(
-            this._service,
-            data.time
-        );
-        location.id = this._service.compatibility.getCreatedEntityIdFromResponse(response);
-        return location;
-    }
-
 }
