aed9017726a1ad495fedf5e04934b896106f91ea
Remy Raes <raes.remy@gmail.com>
2021-10-27 15:00:07 +0200

    [refactor] ObservationType enumeration holds all possible Datastream.observationType values

--- src/model/Datastream.ts
+++ src/model/Datastream.ts
@@ -2,12 +2,13 @@ import {Entity} from "./Entity";
 import {BaseDao} from "../dao/BaseDao";
 import {Polygon} from "@turf/helpers";
 import {SensorThingsService} from "../service/SensorThingsService";
+import {ObservationType} from "./utils/ObservationType";
 
 export class Datastream extends Entity<Datastream> {
     public name: string;
     public description: string;
     public unitOfMeasurement: {name: string, symbol: string, definition: string};
-    public observationType: 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_TruthObservation';
+    public observationType: ObservationType;
     public observedArea: Polygon;
     public phenomenonTime: string;
     public resultTime: string;
@@ -18,8 +19,14 @@ export class Datastream extends Entity<Datastream> {
     //public observedProperties;
     //public observations;
 
-    constructor(service: SensorThingsService, name: string, description: string, unitOfMeasurement: {name: string, symbol: string, definition: string}, observationType: 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CategoryObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_CountObservation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Measurement' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_Observation' | 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_TruthObservation',
-                observedArea: Polygon, phenomenonTime: string, resultTime: string) {
+    constructor(
+        service: SensorThingsService, name: string, description: string,
+        unitOfMeasurement: {name: string, symbol: string, definition: string},
+        observationType: ObservationType,
+        observedArea: Polygon,
+        phenomenonTime: string,
+        resultTime: string
+    ) {
         super(service);
         this.name = name;
         this.description = description;
