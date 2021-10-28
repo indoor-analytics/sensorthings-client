da29860de47a8389a220e2c0070e7f8078736e87
Remy Raes <raes.remy@gmail.com>
2021-10-28 10:56:38 +0200

    [fix] DatastreamDao implements BaseDao.buildEntity member

--- src/dao/DatastreamDao.ts
+++ src/dao/DatastreamDao.ts
@@ -1,13 +1,12 @@
 import {BaseDao} from "./BaseDao";
 import {Datastream} from "../model/Datastream";
-import {AxiosResponse} from "axios";
 import {UnitOfMeasurement} from "../model/utils/UnitOfMeasurement";
 import {ObservationType} from "../model/utils/ObservationType";
 import {Polygon} from "@turf/helpers";
 
 export class DatastreamDao extends BaseDao<Datastream> {
-    buildEntityFromSensorThingsAPIRawData(data: Record<string, unknown>): Datastream {
-        const datastream = new Datastream(
+    buildEntity(data: Record<string, unknown>): Datastream {
+        return new Datastream(
             this._service,
             data.name as string,
             data.description as string,
@@ -17,32 +16,11 @@ export class DatastreamDao extends BaseDao<Datastream> {
             data.phenomenonTime as string,
             data.resultTime as string
         );
-        datastream.id = data['@iot.id'] as unknown as number;
-        return datastream;
     }
-
-    buildEntityFromSensorThingsAPIResponse(response: AxiosResponse): Datastream {
-        const data = response.data as Record<string, unknown>;
-        const datastream = new Datastream(
-            this._service,
-            data.name as string,
-            data.description as string,
-            data.unitOfMeasurement as UnitOfMeasurement,
-            data.observationType as ObservationType,
-            data.observedArea as Polygon,
-            data.phenomenonTime as string,
-            data.resultTime as string
-        );
-        datastream.id = this._service.compatibility.getCreatedEntityIdFromResponse(response);
-        return datastream;
-    }
-
     get entityPathname(): string {
         return "Datastreams";
     }
-
     get entityPublicAttributes(): string[] {
         return ['name', 'description', 'unitOfMeasurement', 'observationType', 'observedArea', 'phenomenonTime', 'resultTime'];
     }
-
 }
