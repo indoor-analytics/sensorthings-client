f01117f1d7994177d0ff88247b8e4b70ed21b20e
Remy Raes <contact@remyraes.com>
2021-11-05 09:32:46 +0100

    [refactor] rewriting TimeChecker.checkTimeRange using TimeChecker.checkISODate

--- src/model/utils/TimeChecker.ts
+++ src/model/utils/TimeChecker.ts
@@ -1,20 +1,18 @@
 import { isValidISODateString } from 'iso-datestring-validator';
 
 export class TimeChecker {
+    public checkISODate(date: string): boolean {
+        return isValidISODateString(date);
+    }
+
     public checkTimeRange(range: string): boolean {
         const dates = range.split('/');
         if (dates.length !== 2)
             return false;
-        try {
-            new Date(dates[0]).toISOString();
-            new Date(dates[1]).toISOString();
-        } catch (err) {
-            return false;
-        }
-        return true;
-    }
 
-    public checkISODate(date: string): boolean {
-        return isValidISODateString(date);
+        const firstMemberIsDate = this.checkISODate(dates[0]);
+        const secondMemberIsDate = this.checkISODate(dates[1]);
+
+        return firstMemberIsDate && secondMemberIsDate;
     }
 }
