955947ef87f3566aee02063b24fe34ff7b0dcb54
Remy Raes <contact@remyraes.com>
2021-12-17 22:15:30 +0100

    [feat] adding documentation to Datastream entity

--- src/model/Datastream.ts
+++ src/model/Datastream.ts
@@ -10,23 +10,74 @@ import {DatastreamObservedPropertiesList} from "./list/DatastreamObservedPropert
 import { TimeChecker } from "./utils/TimeChecker";
 
 /**
- * Representation of a SensorThings Datastream entity.
+ * A Datastream groups a collection of Observations measuring the same ObservedProperty and produced by the same Sensor.
+ * 
  * http://docs.opengeospatial.org/is/15-078r6/15-078r6.html#28
  */
 export class Datastream extends Entity<Datastream> {
+    /**
+     * A property provides a label for Datastream entity, commonly a descriptive name.
+     */
     public name: string;
+
+    /**
+     * The description of the Datastream entity.
+     */
     public description: string;
+
+    /**
+     * A JSON Object containing three key-value pairs. The name property presents the full name of the unitOfMeasurement; 
+     * the symbol property shows the textual form of the unit symbol; and the definition contains the URI defining the 
+     * unitOfMeasurement.
+     * The values of these properties SHOULD follow the Unified Code for Unit of Measure (UCUM).
+     */
     public unitOfMeasurement: UnitOfMeasurement;
+
+    /**
+     * The type of Observation (with unique result type), which is used by the service to encode observations.
+     */
     public observationType: ObservationType;
+
+    /**
+     * The spatial bounding box of the spatial extent of all FeaturesOfInterest that belong to the Observations 
+     * associated with this Datastream.
+     */
     public observedArea: Polygon | undefined;
+
+    /**
+     * The temporal interval of the phenomenon times of all observations belonging to this Datastream.
+     */
     public phenomenonTime: string | undefined;
+
+    /**
+     * The temporal interval of the result times of all observations belonging to this Datastream.
+     */
     public resultTime: string | undefined;
 
+
     // TODO add all entities collections
+
+    /**
+     * A Thing has zero-to-many Datastreams. A Datastream entity SHALL only link to a Thing as a collection of Observations.
+     */
     public things: DatastreamThingsList;
-    //public sensors;
+
+    /**
+     * The Observations in a Datastream are performed by one-and-only-one Sensor. One Sensor MAY produce zero-to-many 
+     * Observations in different Datastreams.
+     */
+    // public sensors;
+
+    /**
+     * The Observations of a Datastream SHALL observe the same ObservedProperty. The Observations of different Datastreams 
+     * MAY observe the same ObservedProperty.
+     */
     public observedProperties: DatastreamObservedPropertiesList;
-    //public observations;
+
+    /**
+     * A Datastream has zero-to-many Observations. One Observation SHALL occur in one-and-only-one Datastream.
+     */
+    // public observations;
 
     constructor(
         service: SensorThingsService, name: string, description: string,
