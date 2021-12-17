e88444f5b41d0cca6d234a4301dfa1c4b1addf61
Remy Raes <contact@remyraes.com>
2021-12-17 22:33:52 +0100

    [feat] adding documentation to ObservedProperty entity

--- src/model/ObservedProperty.ts
+++ src/model/ObservedProperty.ts
@@ -4,14 +4,32 @@ import {ObservedPropertyDao} from "../dao/ObservedPropertyDao";
 import {ObservedPropertyDatastreamsLists} from "./list/ObservedPropertyDatastreamsLists";
 
 /**
- * Representation of a SensorThings ObservedProperty entity.
+ * An ObservedProperty specifies the phenomenon of an Observation.
+ * 
  * https://docs.opengeospatial.org/is/15-078r6/15-078r6.html#30
  */
 export class ObservedProperty extends Entity<ObservedProperty> {
+    /**
+     * A property provides a label for ObservedProperty entity, commonly a descriptive name.
+     */
     public name: string;
+
+    /**
+     * The URI of the ObservedProperty. Dereferencing this URI SHOULD result in a representation of the definition 
+     * of the ObservedProperty.
+     */
     public definition: string;
+
+    /**
+     * A description about the ObservedProperty.
+     */
     public description: string;
 
+
+    /**
+     * The Observations of a Datastream observe the same ObservedProperty. The Observations of different Datastreams 
+     * MAY observe the same ObservedProperty.
+     */
     public datastreams: ObservedPropertyDatastreamsLists;
 
     constructor(
