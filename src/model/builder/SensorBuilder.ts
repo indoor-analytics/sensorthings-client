0f1712fd8fea8b9e7e5b21e037994c5df570705b
Remy Raes <raes.remy@gmail.com>
2022-01-10 13:43:34 +0100

    [fix] SensorBuilder throws if a required attribute is not given

--- src/model/builder/SensorBuilder.ts
+++ src/model/builder/SensorBuilder.ts
@@ -1,5 +1,6 @@
 import {AbstractBuilder} from "./AbstractBuilder";
 import {Sensor} from "../Sensor";
+import {MissingArgumentError} from "../../error/MissingArgumentError";
 
 export class SensorBuilder extends AbstractBuilder<Sensor> {
     public setName(name: string): SensorBuilder {
@@ -20,6 +21,15 @@ export class SensorBuilder extends AbstractBuilder<Sensor> {
     }
 
     protected buildEntity(): Sensor {
+        if (!this._attributes.name)
+            throw new MissingArgumentError('"name" argument is required to build a Sensor.');
+        if (!this._attributes.description)
+            throw new MissingArgumentError('"description" argument is required to build a Sensor.');
+        if (!this._attributes.encodingType)
+            throw new MissingArgumentError('"encodingType" argument is required to build a Sensor.');
+        if (!this._attributes.metadata)
+            throw new MissingArgumentError('"metadata" argument is required to build a Sensor.');
+
         return new Sensor(
             this._service,
             this._attributes.name as string,
