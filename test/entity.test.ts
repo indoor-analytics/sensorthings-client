d8b7eea4fe78ce03563b78a6c81c594ba882127c
Remy Raes <raes.remy@gmail.com>
2022-01-10 15:26:38 +0100

    [test] Datastream should not be created with unit.definition === null

--- test/entity.test.ts
+++ test/entity.test.ts
@@ -141,6 +141,17 @@ describe('Datastream', () => {
         );
         expect(create).toThrowError(new RangeError('unitOfMeasurement must have all of its properties either defined or null.'));
     });
+
+    it('should not create an instance with unit.definition null only', () => {
+        const create = () => new Datastream(
+            service, 'name', 'description',
+            {name: 'name', definition: null, symbol: 'S'}, // unitOfMeasurement
+            ObservationType.OM_CategoryObservation, area,
+            "2014-03-01T13:00:00Z/2015-05-11T15:30:00Z",
+            "2014-03-01T13:00:00Z/2015-05-11T15:30:00Z"
+        );
+        expect(create).toThrowError(new RangeError('unitOfMeasurement must have all of its properties either defined or null.'));
+    });
 });
 
 describe ('Observation', () => {
