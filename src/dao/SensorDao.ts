12f916c00d5cb2a0c86633b6271bd400c44df68e
Remy Raes <raes.remy@gmail.com>
2022-01-10 12:11:08 +0100

    [feat] SensorDao can build Sensors through buildEntity method

--- src/dao/SensorDao.ts
+++ src/dao/SensorDao.ts
@@ -8,7 +8,13 @@ export class SensorDao extends BaseDao<Sensor> {
     get entityPublicAttributes(): string[] {
         return ['name', 'description', 'encodingType', 'metadata'];
     }
-    buildEntity(_: Record<string, unknown>): Sensor {
-        throw new Error("Method not implemented.");
+    buildEntity(data: Record<string, unknown>): Sensor {
+        return new Sensor(
+            this._service,
+            data.name as string,
+            data.description as string,
+            data.encodingType as string,
+            data.metadata as string
+        );
     }
 }
